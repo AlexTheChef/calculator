1ecfe4c12ee6b8994832e19bd90779b6aa27f796
XxMaxFateXx <aleksa.kumric@gmail.com>
2021-10-11 12:41:04 +0200

    all design aspects are finished and added js for theme switch

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 const $tabs = document.querySelectorAll('.js-tab')
 
-const colorTheme = [
-    {
-        theme: 'dark'
-    },
-    {
-        theme: 'light'
-    },
-    {
-        theme: 'saturated'
-    }
-]
+const colorTheme = ['dark', 'light', 'saturated']
 
 function slide(index) {
     $tabs.forEach($tab => $tab.classList.remove('active-tab'))
-    $tabs[index-1].classList.add('active-tab')
+    $tabs[index - 1].classList.add('active-tab')
 }
 
 $tabs.forEach(($tab, index) => $tab.addEventListener("click", () => {
-    slide(index+1)
-    document.documentElement.setAttribute('theme', colorTheme[index].theme);
+    slide(index + 1)
+    document.documentElement.setAttribute('theme', colorTheme[index]);
 }))
